5191197834f18abffd8ae925d1d8840211691797
Pavel Montes <pavelmontes22@gmail.com>
2023-04-03 17:11:34 -0600

    T1.4: Verificacion de inputs en el componente Signup

--- src/app/authentication/signup/signup.component.ts
+++ src/app/authentication/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -11,6 +11,12 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class SignupComponent {
   signUpForm: FormGroup =  new FormGroup("");
+  invalidUsername: boolean | undefined = false;
+  invalidEmail: boolean | undefined = false;
+  invalidPassword: boolean | undefined = false;
+  invalidConfirmPassword: boolean | undefined  = false;
+  invalidPasswordsMatch: boolean | undefined = false;
+  userExists: boolean = false;
 
   constructor(
     private fb: FormBuilder,
@@ -25,11 +31,29 @@ export class SignupComponent {
       username: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(8)]],
-      confirmPassword: ['', Validators.required],
+      confirmPassword: ['', [Validators.required, this.passwordConfirming]],
     });
   }
 
+  private passwordConfirming(control: AbstractControl): any {
+    if (control && control.parent) {
+      const password = control.parent.get('password');
+      const confirmPassword = control.parent.get('confirmPassword');
+      if (!password || !confirmPassword) {
+        return null;
+      }
+      return password.value === confirmPassword.value ? null : { passwordNotMatch: true };
+    }
+    return null;
+  }
+
   submitSignUpForm(){
+    this.invalidEmail = this.signUpForm.get('email')?.invalid;
+    this.invalidUsername = this.signUpForm.get('username')?.invalid;
+    this.invalidPassword = this.signUpForm.get('password')?.invalid;
+    this.invalidConfirmPassword = this.signUpForm.get('confirmPassword')?.invalid;
+    this.userExists = false;
+
     if(this.signUpForm.valid){
       const username = this.signUpForm.get('username')?.value;
       const email = this.signUpForm.get('email')?.value;
@@ -41,10 +65,11 @@ export class SignupComponent {
       this.authService.signUp(username, email, password)
       .subscribe(
         (response) => {
-          console.log(response);
+          this.userExists = false;
         },
         (error) => {
-          console.log(error);
+          console.error(error);
+          this.userExists = true;
         }
       );
     }

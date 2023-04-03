40c907f105a8dcc2c4f1526838351a51474824f7
Pavel Montes <pavelmontes22@gmail.com>
2023-04-03 16:54:15 -0600

    T2.2: Funcionamiento en el componente signup

--- src/app/authentication/signup/signup.component.ts
+++ src/app/authentication/signup/signup.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +10,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
+  signUpForm: FormGroup =  new FormGroup("");
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.createForm();
+  }
+
+  createForm() {
+    this.signUpForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', Validators.required],
+    });
+  }
+
+  submitSignUpForm(){
+    if(this.signUpForm.valid){
+      const username = this.signUpForm.get('username')?.value;
+      const email = this.signUpForm.get('email')?.value;
+      const password = this.signUpForm.get('password')?.value;
+      console.log("username: " , username);
+      console.log("email: " , email);
+      console.log("password: " , password);
+
+      this.authService.signUp(username, email, password)
+      .subscribe(
+        (response) => {
+          console.log(response);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../models/user.model'
 import { Auth } from 'aws-amplify';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,37 @@ export class AuthService {
   private user: User = { token: "", email: ""};
   private userExists: boolean = false;
 
-  constructor() { }
+  constructor(
+    private router:Router
+  ) { }
+
+  //Sign Up
+  signUp(username:string, email: string, password:string):Observable<string>{
+    return new Observable(observer => {
+      Auth.signUp({
+        username,
+        password,
+        attributes: {
+          email,
+          nickname: username
+        },
+      }).then(
+        (response) => {
+          this.user = this.saveUser("", email)
+          this.logged = true;
+          this.router.navigate(['auth/verification'])
+
+          observer.next('success');
+          observer.complete();
+        },
+        (error) => {
+          this.logged = false;
+          this.userExists = true;
+          observer.error(error);
+        }
+      );
+    });
+  }
 
   // SignIn
   signIn(username:string, password:string):Observable<string> {

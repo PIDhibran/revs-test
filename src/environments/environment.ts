4597bb1c15c6567b3b12ebff95c74ad9627796f5
Pavel Montes <pavelmontes22@gmail.com>
2023-04-03 15:43:32 -0600

    Configuracion del proyecto para el uso de amplify

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  aws: {
+    "aws_project_region": "us-east-1",
+    "aws_cognito_region": "us-east-1",
+    "aws_user_pools_id": "us-east-1_DKkivL7ew",
+    "aws_user_pools_web_client_id": "6jh9su13201fm67i6utsvkopio"
+  },
+  s3Bucket: '',
+  apiUrl: '',
+};
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { Amplify } from 'aws-amplify';
+import { environment } from './environments/environment';
 
+Amplify.configure(environment.aws);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));

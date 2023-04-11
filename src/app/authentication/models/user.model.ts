f1d083308e66cfd1cc0e99268538e58e72589586
Pavel Montes <pavel.montes@pidelectronics.com>
2023-04-11 11:48:01 -0700

    fix config

---
+++ src/app/authentication/models/user.model.ts
@@ -0,0 +1,10 @@
+export interface User {
+  token:string;
+  email:string;
+  username: string;
+}
+
+export interface Response{
+  body:any;
+  statusCode:string;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+
 import { AppModule } from './app/app.module';
-import { Amplify } from 'aws-amplify';
+import { Amplify, Auth } from 'aws-amplify';
 import { environment } from './environments/environment';
 
 Amplify.configure(environment.aws);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+Auth.configure(environment.aws);
